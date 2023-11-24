fb654843e44c4f19aac912e58292862c3b3d3913
DHANUSHA AKHILRAJ <dhanusha.c@kgcas.com>
2023-11-24 16:07:12 +0530

    Zone

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-dhan-project-5678"
   location = "europe-west2"
 }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-dhan-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-dhan-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
